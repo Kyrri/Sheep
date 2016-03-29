1ebf8e3360a78cd3ef07ee627393c6d999d26a35
Lindsay Paterson <lindsaymrjpaterson@gmail.com>
2016-03-29 18:11:05 -0400

    Initial Commit

---
+++ Assets/Scripts/SheepFear.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SheepFear : MonoBehaviour {
+	public GameObject player;
+	bool fear = false;
+	float maxSpeed = 20;
+	// Use this for initialization
+	void Start () {
+		//player = GameObject.FindWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float playerDist = Vector3.Distance (player.transform.position, transform.position);
+		if (playerDist < 14) {
+			fear = true;
+			fleePlayer ();
+		} else {
+			if (fear == true) {
+				fear = false;
+			}
+		}
+	}
+	void fleePlayer(){
+		Vector3 direction = transform.position - player.transform.position;
+		direction.Normalize ();
+		float speed = maxSpeed - Vector3.Distance (player.transform.position, transform.position);
+		float step = speed * Time.deltaTime;
+		transform.Translate (direction.x * step, 0, direction.z*step);
+	}
+	public bool fearStatus(){
+		return fear;
+	}
+}
---
+++ Assets/Scripts/SheepGrouping.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SheepGrouping : MonoBehaviour {
+	public float speed = 10;
+	public GameObject mainSheep;
+	SheepFear fearScript;
+
+	void Start() {
+		fearScript = gameObject.GetComponent<SheepFear>();
+	}
+
+	void Update () {
+		float sheepDist = Vector3.Distance (mainSheep.transform.position, transform.position);
+		if(sheepDist >8 && !fearScript.fearStatus()) {
+			cozyUp ();
+		}
+	}
+	void cozyUp(){
+		Vector3 direction =  mainSheep.transform.position - transform.position;
+		direction.Normalize ();
+		float step = speed * Time.deltaTime;
+		transform.Translate (direction.x * step, 0, direction.z*step);
+
+	}
+}
